61132f1a50e07df6109f8fd1c6b0a62ea74b4ad9
Karl Bristav <karl.bristav@gmail.com>
2024-12-05 07:41:32 +0100

    Day 5

---
+++ AoC2024/Day05.fs
@@ -0,0 +1,99 @@
+﻿namespace AoC2024
+
+open Utils
+
+module Day05 =
+
+    let parseInput (input: string) : ((int * int) array) * int array array =
+        let (rules, updates) = input |> splitAtDoubleLines |> twoArrayToTuple
+
+        let processedRules =
+            rules
+            |> lines
+            |> Array.map (split "|")
+            |> Array.map (Array.map int)
+            |> Array.map twoArrayToTuple
+
+        let processedUpdates =
+            updates
+            |> lines
+            |> Array.map (commas >> (Array.map int))
+
+        (processedRules, processedUpdates)
+
+    let pickRulesForUpdate (rules: (int * int) array) (update: int array) : (int * int) array =
+        rules
+        |> Array.filter (fun (a, b) ->
+            (Array.contains a update)
+            && (Array.contains b update))
+
+    let isUpdateCorrect (rules: (int * int) array) (update: int array) : bool =
+        rules
+        |> Array.map (fun (a, b) -> Array.findIndex ((=) a) update < Array.findIndex ((=) b) update)
+        |> Array.forall id
+
+    let getMiddlePageNumber (update: int array) : int = update[(update.Length - 1) / 2]
+
+    let part1 (input: string) : string =
+        let (rules, updates) = input |> parseInput
+
+        updates
+        |> Array.filter (fun update ->
+            let relevantRules = pickRulesForUpdate rules update
+            isUpdateCorrect relevantRules update)
+        |> Array.map getMiddlePageNumber
+        |> Array.sum
+        |> string
+
+    let makePairs (update: int array) : ((int * int) * (int * int)) array =
+        update
+        |> Array.tail
+        |> Array.mapi (fun i b -> (0, update[0]), (i + 1, b))
+
+    let swapValues (array: 'a array) (indexA: int) (indexB: int) : 'a array =
+        let newArray = Array.map id array
+        let aVal = array[indexA]
+        let bVal = array[indexB]
+        Array.set newArray indexA bVal
+        Array.set newArray indexB aVal
+
+        newArray
+
+    let swapIncorrect (rules: (int * int) array) (update: int array) : int array =
+        let pairs = makePairs update
+
+        let badPairOption =
+            pairs
+            |> Array.tryFind (fun ((_, a), (_, b)) -> rules |> Array.contains (b, a))
+
+        match badPairOption with
+        | None -> update
+        | Some ((iA, _), (iB, _)) -> swapValues update iA iB
+
+    let rec sortPass (rules: (int * int) array) (update: int array) : int array =
+        match update with
+        | [||] -> [||]
+        | [| a |] -> [| a |]
+        | arr ->
+            let swapped = swapIncorrect rules arr
+            Array.append [| swapped[0] |] (swapped |> Array.tail |> sortPass rules)
+
+    let rec sortUntilCorrect (rules: (int * int) array) (update: int array) : int array =
+        if isUpdateCorrect rules update then
+            update
+        else
+            sortUntilCorrect rules (sortPass rules update)
+
+    let part2 (input: string) : string =
+        let (rules, updates) = input |> parseInput
+
+        updates
+        |> Array.filter (fun update ->
+            let relevantRules = pickRulesForUpdate rules update
+            not (isUpdateCorrect relevantRules update))
+        |> Array.map (fun update ->
+            let relevantRules = pickRulesForUpdate rules update
+            sortUntilCorrect relevantRules update)
+        |> Array.map getMiddlePageNumber
+        |> Array.sum
+        |> string
--- AoC2024/TestRunner.fs
+++ AoC2024/TestRunner.fs
@@ -101,3 +101,14 @@ module TestRunner =
     let examples4_2 = examples4_1
 
     let exampleResults4_2 = [| "9" |]
+
+    // Day5
+    let input5 = getInput 5
+
+    let examples5_1 = [| "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47" |]
+
+    let exampleResults5_1 = [| "143" |]
+
+    let examples5_2 = examples5_1
+
+    let exampleResults5_2 = [| "123" |]
