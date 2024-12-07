8cec68b24612ffec8624bf478b2a10584fcae772
Karl Bristav <karl.bristav@gmail.com>
2024-12-07 20:51:01 +0100

    Day 7

---
+++ AoC2024/Day07.fs
@@ -0,0 +1,74 @@
+﻿namespace AoC2024
+
+open Utils
+
+module Day07 =
+    let parseInput (input: string) : (int64 * (int64 array)) array =
+        input
+        |> lines
+        |> Array.map (fun line ->
+            let (a, b) = line |> colons |> twoArrayToTuple
+            (int64 a, b |> words |> Array.map int64))
+
+    let rec makeOperatorLists (useHidden: bool) (acc: char list list) (length: int) : char list list =
+        if length = 0 then
+            acc
+        else
+            let lists =
+                if useHidden then
+                    if acc.IsEmpty then
+                        [ [ '+' ]; [ '*' ]; [ '|' ] ]
+                    else
+                        acc |> List.collect (fun l -> [ '+' :: l; '*' :: l; '|' :: l ])
+                else if acc.IsEmpty then
+                    [ [ '+' ]; [ '*' ] ]
+                else
+                    acc |> List.collect (fun l -> [ '+' :: l; '*' :: l ])
+
+            makeOperatorLists useHidden lists (length - 1)
+
+    let rec applyOperators
+        (target: int64)
+        (useHidden: bool)
+        (acc: int64)
+        (operators: char list)
+        (operands: int64 list)
+        : int64 =
+        if acc > target then
+            -1
+        else
+            match operators, operands with
+            | [], _ -> acc
+            | (op :: ops, n :: ns) ->
+                if op = '+' then
+                    applyOperators target useHidden (acc + n) ops ns
+                elif op = '*' then
+                    applyOperators target useHidden (acc * n) ops ns
+                else
+                    applyOperators target useHidden (int64 $"{acc}{n}") ops ns
+
+    let canMakeEquation (useHidden: bool) ((total, operands): (int64 * (int64 array))) : bool =
+        let operatorsLists =
+            makeOperatorLists useHidden [] (operands.Length - 1) |> Array.ofList
+
+        let o :: os = operands |> Array.toList
+
+        operatorsLists
+        |> Array.map (fun operatorList -> applyOperators total useHidden o operatorList os)
+        |> Array.exists ((=) total)
+
+    let part1 (input: string) : string =
+        input
+        |> parseInput
+        |> Array.filter (canMakeEquation false)
+        |> Array.map fst
+        |> Array.sum
+        |> string
+
+    let part2 (input: string) : string =
+        input
+        |> parseInput
+        |> Array.filter (canMakeEquation true)
+        |> Array.map fst
+        |> Array.sum
+        |> string
--- AoC2024/TestRunner.fs
+++ AoC2024/TestRunner.fs
@@ -123,3 +123,14 @@ module TestRunner =
     let examples6_2 = examples6_1
 
     let exampleResults6_2 = [| "6" |]
+
+    // Day7
+    let input7 = getInput 7
+
+    let examples7_1 = [| "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20" |]
+
+    let exampleResults7_1 = [| "3749" |]
+
+    let examples7_2 = examples7_1
+
+    let exampleResults7_2 = [| "11387" |]
